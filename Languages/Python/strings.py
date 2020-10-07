fa09648e072fd128992079153810741c0ee2d83b
wenses penadillo lazares <wenses400@gmail.com>
2020-10-07 15:50:21 -0500

    Update strings.py

--- Languages/Python/strings.py
+++ Languages/Python/strings.py
@@ -6,3 +6,7 @@ print(phrase.upper())
 print(phrase.islower())
 print(phrase.upper().isupper())
 print(len(phrase))
+print(phrase[4]) # 'n'
+print(phrase.index("r")) # 3
+print(phrase.index("Py")) # 9
+print(phrase.replace("Learning", "Working whit"))

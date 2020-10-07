f6e09bc9bf7ed4c0dbeda56d1882c4be0f1b1823
wenses penadillo lazares <wenses400@gmail.com>
2020-10-07 13:43:49 -0500

    Update variables_and_data_types.py

--- Languages/Python/variables_and_data_types.py
+++ Languages/Python/variables_and_data_types.py
@@ -1,12 +1,15 @@
-string = "Hello World!!" #str
+var5 = "Hello World!!" #str
 var1 = 'a' #str
 var2 = 12 #int
+var4 = 3.5 #float
 var3 = False #bool
-print (string)
-print(type(string))
+print(var5)
+print(type(var5))
 print(var1)
 print(type(var1))
 print(var2)
 print(type(var2))
+print(var4)
+print(type(var4))
 print(var3)
 print(type(var3))

109668214a3f905c7490b4929c6acad2c7a51794
wenses penadillo lazares <wenses400@gmail.com>
2020-10-07 15:39:20 -0500

    Update strings.py

--- Languages/Python/strings.py
+++ Languages/Python/strings.py
@@ -1,3 +1,8 @@
 phrase = "Learning Python"
 
+print(phrase + " is awesome!!")
 print(phrase.lower())
+print(phrase.upper())
+print(phrase.islower())
+print(phrase.upper().isupper())
+print(len(phrase))

f1f67bf5a028ff344d37072f722745c8e0bc97ac
wenses penadillo lazares <wenses400@gmail.com>
2020-10-07 13:55:15 -0500

    Update variables_and_data_types.py

--- Languages/Python/variables_and_data_types.py
+++ Languages/Python/variables_and_data_types.py
@@ -1,15 +1,11 @@
-var5 = "Hello World!!" #str
-var1 = 'a' #str
-var2 = 12 #int
-var4 = 3.5 #float
-var3 = False #bool
-print(var5)
-print(type(var5))
-print(var1)
-print(type(var1))
-print(var2)
-print(type(var2))
-print(var4)
-print(type(var4))
-print(var3)
-print(type(var3))
+var5 = "Hello World!!" # str
+var1 = 'a' # str
+var2 = 12 # int
+var4 = 3.5 # float
+var6 = 4,7 # tuple
+var3 = False # bool
+
+variables = [var1, var2, var3, var4, var5, var6]
+
+for v in variables:
+    print("\' " + str(v) + " \'" + " is " + str(type(v)))

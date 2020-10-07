004ad3ee00ebd7eb311bb3e61648413507c25ef3
wenses penadillo lazares <wenses400@gmail.com>
2020-10-07 13:39:21 -0500

    Update variables_and_data_types.py

---
+++ Languages/Python/variables_and_data_types.py
@@ -0,0 +1,12 @@
+string = "Hello World!!" #str
+var1 = 'a' #str
+var2 = 12 #int
+var3 = False #bool
+print (string)
+print(type(string))
+print(var1)
+print(type(var1))
+print(var2)
+print(type(var2))
+print(var3)
+print(type(var3))

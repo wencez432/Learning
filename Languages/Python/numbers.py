468cf5cf1d91f1f8f8371999d3670277286a9454
wenses penadillo lazares <wenses400@gmail.com>
2020-10-07 16:17:05 -0500

    Update numbers.py

--- Languages/Python/numbers.py
+++ Languages/Python/numbers.py
@@ -4,3 +4,16 @@ print(3 + 4.9) # 7.9
 print(34 / 0.32) # 106.25
 print(2 * 5 + 9) # 19
 print(2 * (5 + 9)) # 28
+
+num = -3
+
+print(num)
+print(str(num) + " is a number")
+
+print(abs(num)) # |-3| = 3
+print(pow(4, 6)) # 4^6 = 4096
+print(max(6, 29)) # 29
+print(min(3, -2)) # -2
+print(round(4.50)) # 4
+print(round(4.51)) # 5
+print(round(4.5551, 2)) # 4.56

d645149be83bef0ed85c6e6a30726ce6e1187c50
wenses penadillo lazares <wenses400@gmail.com>
2020-10-08 16:47:37 -0500

    Update functions.py

--- Languages/Python/functions.py
+++ Languages/Python/functions.py
@@ -1,4 +1,8 @@
-def say_hi(name):
-    print("Hi " + name)
+def say_hi(name, age):
+    print("Hi " + name + " , you are " + str(age))
 
-say_hi("John")
+def cube(num):
+    return num*num*num
+
+say_hi("John", 32)
+print(cube(2))
